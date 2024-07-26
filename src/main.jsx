5304a247818f0b511d983f6cebba5c5ff91af613
Ember Borja <eborja@bvhllc.com>
2024-07-26 15:38:25 -0400

    Fixed routing, filter still not working 100%

--- src/main.jsx
+++ src/main.jsx
@@ -26,26 +26,44 @@ import HomePage from "./HomePage.jsx"
 
 function Layout() {
   return (
-    <>
+    <div id="app">
       <Header />
-      <div id="page-content">
+      <main id="page-content">
         <Outlet />
+      </main>
+      <div className="water-effect">
+        <div className="water"></div>
+        <svg>
+          <filter id="turbulence" x="0" y="0" width="100%" height="100%">
+            <feTurbulence id="water-filter" numOctaves="3"></feTurbulence>
+            <feDisplacementMap
+              in="SourceGraphic"
+              scale="20"
+            ></feDisplacementMap>
+            <animate
+              xlinkHref="#water-filter"
+              attributeName="baseFrequency"
+              dur="30s"
+              keyTimes="0;1"
+              values="0.03;0.06"
+              repeatCount="indefinite"
+            />
+          </filter>
+        </svg>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
 const router = createBrowserRouter([
   {
-    element: <Outlet />,
+    element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
-        element: (
-            <HomePage />
-        ),
+        element: <HomePage />,
       },
       {
         path: "/login",
